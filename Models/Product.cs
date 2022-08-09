e899731f3b7a312073783e830b8a1b621652fcf3
KNJoabRomero <jose.romero@kinandcarta.com>
2022-08-09 15:35:29 -0500

    feat: added product api

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ProductApi
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public string? Description { get; set; }
+    }
+}
